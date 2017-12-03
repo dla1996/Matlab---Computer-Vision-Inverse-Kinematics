9d637e58f09d32afb6c6a086c91563abf3f8dfb5
dla1996 <denwisla@yahoo.com>
2017-12-02 22:27:29 -0600

    Add files via upload

---
+++ docs/Scripts for Inverse Kinematics Solving/inverseKinematics.m
@@ -0,0 +1,17 @@
+a = inverseFunction(1,2,3)
+
+function [theta1 theta2 theta3] = inverseFunction(x, y, z)
+    l1 = 5; % shoulder to elbow inches
+    l2 = 5; % elbow to wrist inches
+    l3 = 3; % wrist to end effector center inches
+    
+    Rotation = [0 1 0
+                -1 0 0
+                0 0 1];
+    
+    originWrist = [x y z] - l3*Rotation * transpose([ 0 0 1]);
+    theta1 = atan2(yWrist, xWrist);
+    theta2 = 
+    
+    
+end
