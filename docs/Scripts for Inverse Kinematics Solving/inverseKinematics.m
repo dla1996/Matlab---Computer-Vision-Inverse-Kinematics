a3314f2a316dbc71c66478e726393638ef99ace6
dla1996 <denwisla@yahoo.com>
2017-12-02 22:42:13 -0600

    Update inverseKinematics.m

--- docs/Scripts for Inverse Kinematics Solving/inverseKinematics.m
+++ docs/Scripts for Inverse Kinematics Solving/inverseKinematics.m
@@ -7,7 +7,7 @@ function [theta1 theta2 theta3] = inverseFunction(x, y, z)
                 -1 0 0
                 0 0 1];
     
-    originWrist = [x y z] - L3*Rotation * transpose([0 0 1]);
+    originWrist = transpose([x y z]) - L3*Rotation * transpose([0 0 1]);    % This will give a 3x1 matrix of wrist origin
     theta1 = atan2(yWrist, xWrist);
     theta2 = 
     
